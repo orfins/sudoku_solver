1dadc015485ec5dc0cfa9d92773f7833b93a908f
Or Fins <or@octopol.io>
2021-06-26 12:03:38 +0300

    added structures.py

---
+++ sudoku_solver/structures.py
@@ -0,0 +1,137 @@
+from itertools import chain
+from math import sqrt
+from typing import Union
+
+e = '_'
+
+
+class Cell:
+    def __init__(self, board, row_i, col_i, value):
+        self.__board = board
+        self.__row_i = row_i
+        self.__col_i = col_i
+        self.__value = value
+
+        self.possible_values = []  # For the algorithms to play with.
+
+        self.__square = None
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, v: Union[int, str]):
+        self.__value = str(v)
+        self.possible_values = []
+
+        for cell in chain(self.row, self.col, self.square):
+            if v in cell.possible_values:
+                cell.possible_values.remove(v)
+
+    @property
+    def row(self):
+        return self.__board.rows[self.__row_i]
+
+    @property
+    def col(self):
+        return self.__board.cols[self.__col_i]
+
+    @property
+    def indexes(self):
+        return self.__row_i, self.__col_i
+
+    @property
+    def square(self):
+        if not self.__square:
+            square_size = int(sqrt(len(self.row)))
+
+            first_row = int(self.__row_i / square_size) * square_size
+            last_row = first_row + square_size
+
+            first_col = int(self.__col_i / square_size) * square_size
+            last_col = first_col + square_size
+
+            s = []
+            for r in range(first_row, last_row):
+                for c in range(first_col, last_col):
+                    s.append(self.__board.rows[r][c])
+
+            self.__square = tuple(s)
+
+        return self.__square
+
+    def __eq__(self, other: Union[int, str]):
+        return self.__value == str(other)
+
+    def __repr__(self):
+        return str(self.value)
+
+    def is_empty(self):
+        return self.value == e
+
+    def can_fill(self, value):
+        return value in self.possible_values
+
+    def update_possible_values(self):
+        self.possible_values = []
+
+        if self.is_empty():
+            for digit in self.__board.digits:
+                if digit not in self.row and digit not in self.col and digit not in self.square:
+                    self.possible_values.append(digit)
+
+
+class Board:
+    def __init__(self, values=None):
+        board_size = len(values)
+
+        self.__board = tuple(
+            tuple(Cell(self, row_index, col_index, str(values[row_index][col_index]))
+                  for col_index in range(board_size))
+            for row_index in range(board_size))
+
+        self.__cols = tuple(
+            tuple(row[col] for row in self.rows) for col in range(len(self.__board))
+        )
+        self.__cells = tuple(cell for row in self.rows for cell in row)
+
+        for cell in self.cells:
+            cell.update_possible_values()
+
+    @property
+    def rows(self):
+        return self.__board
+
+    @property
+    def cols(self):
+        return self.__cols
+
+    @property
+    def cells(self):
+        return self.__cells
+
+    @property
+    def digits(self):
+        return range(1, 10)
+
+    def __repr__(self):
+        return '\n'.join(
+            ''.join(str(cell) for cell in row) for row in self.rows
+        )
+
+    def at(self, row, col):
+        return self.rows[row][col]
+
+    def copy(self):
+        values = [[cell.value for cell in row] for row in self.rows]
+
+        copied = Board(values)
+
+        return copied
+
+    def is_solvable(self):
+        return all((not cell.is_empty()) or len(cell.possible_values) > 0 for cell in self.cells)
+
+    def is_complete(self):
+        return all(not cell.is_empty() for cell in self.cells)
---
+++ sudoku_solver/tests/test_structures.py
@@ -0,0 +1,138 @@
+from sudoku_solver.structures import Board, e
+from .boards import board_values_by_difficulty
+
+
+def test_boards_properties_are_built_correctly():
+    for values in board_values_by_difficulty.values():
+        board = Board(values)
+        for row_i in range(len(values)):
+            for col_i in range(len(values)):
+                assert board.rows[row_i][col_i] == values[row_i][col_i]
+                assert board.cols[col_i][row_i] == values[row_i][col_i]
+                assert board.at(row_i, col_i) == values[row_i][col_i]
+
+
+def test_cells_retrieve_correct_rows():
+    for values in board_values_by_difficulty.values():
+        board = Board(values)
+
+        for row in board.rows:
+            for cell in row:
+                assert row == cell.row
+
+
+def test_cells_retrieve_correct_cols():
+    for values in board_values_by_difficulty.values():
+        board = Board(values)
+
+        for col in board.cols:
+            for cell in col:
+                assert col == cell.col
+
+
+def test_cells_retrieve_correct_square():
+    board = Board(board_values_by_difficulty['easy1'])
+
+    square = board.at(3, 5).square
+    assert len(square) == 9
+    assert board.at(3, 3) in square
+    assert board.at(3, 4) in square
+    assert board.at(3, 5) in square
+    assert board.at(4, 3) in square
+    assert board.at(4, 4) in square
+    assert board.at(4, 5) in square
+    assert board.at(5, 3) in square
+    assert board.at(5, 4) in square
+    assert board.at(5, 5) in square
+
+    assert board.at(3, 3).square == square
+    assert board.at(3, 4).square == square
+    assert board.at(3, 5).square == square
+    assert board.at(4, 3).square == square
+    assert board.at(4, 4).square == square
+    assert board.at(4, 5).square == square
+    assert board.at(5, 3).square == square
+    assert board.at(5, 4).square == square
+    assert board.at(5, 5).square == square
+
+
+def test_cells_indexes_match_their_location():
+    for values in board_values_by_difficulty.values():
+        board = Board(values)
+
+        for cell in board.cells:
+            assert board.at(*cell.indexes) == cell
+
+
+def test_cells_is_empty():
+    for values in board_values_by_difficulty.values():
+        board = Board(values)
+
+        for cell in board.cells:
+            assert (cell.value == e) == cell.is_empty()
+
+
+def test_cells_can_fill():
+    for values in board_values_by_difficulty.values():
+        board = Board(values)
+
+        for cell in board.cells:
+            for digit in board.digits:
+                if cell.is_empty():
+                    assert cell.can_fill(digit) == (
+                            digit not in cell.row and
+                            digit not in cell.col and
+                            digit not in cell.square)
+                else:
+                    assert not cell.can_fill(digit)
+
+
+def test_cells_update_possible_values():
+    board = Board(board_values_by_difficulty['easy1'])
+
+    board.at(0, 1).update_possible_values()
+    assert board.at(0, 1).possible_values == []
+
+    board.at(0, 0).update_possible_values()
+    assert board.at(0, 0).possible_values == [5]
+
+    board.at(0, 2).update_possible_values()
+    assert board.at(0, 2).possible_values == [7]
+
+    board.at(1, 2).update_possible_values()
+    assert board.at(1, 2).possible_values == [6, 7]
+
+    board.at(6, 2).update_possible_values()
+    assert board.at(6, 2).possible_values == [2, 6, 9]
+
+
+def test_boards_update_possible_values():
+    board = Board(board_values_by_difficulty['easy1'])
+
+    assert board.at(0, 1).possible_values == []
+    assert board.at(0, 0).possible_values == [5]
+    assert board.at(6, 2).possible_values == [2, 6, 9]
+
+
+def test_cells_update_possible_values_when_assigned():
+    board = Board(board_values_by_difficulty['expert1'])
+
+    assert board.at(0, 0).possible_values == [3, 6, 8]
+    assert board.at(0, 8).possible_values == [1, 3]
+
+    board.at(0, 0).value = 3
+
+    assert board.at(0, 0).possible_values == []
+    assert board.at(0, 8).possible_values == [1]
+
+
+def test_board_copy_is_deep():
+    for values in board_values_by_difficulty.values():
+        board = Board(values)
+        copied = board.copy()
+
+        assert board is not copied
+        for row_i in range(len(values)):
+            for col_i in range(len(values)):
+                assert board.at(row_i, col_i) == copied.at(row_i, col_i)
+                assert board.at(row_i, col_i) is not copied.at(row_i, col_i)
